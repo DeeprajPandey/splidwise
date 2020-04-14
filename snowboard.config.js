c44b0c54d7d70569f34f6464215539eee3c6fc95
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-15 02:37:43 +0530

    Change doc generating tool

---
+++ snowboard.config.js
@@ -0,0 +1,18 @@
+const { resolve } = require("path");
+
+module.exports = {
+  html: {
+    playground: {
+      enabled: true,
+      env: "development",
+      environments: {
+        // development: {
+        //   url: "http://localhost:6401/",
+        // },
+        staging: {
+          url: "https://fathomless-fortress-82121.herokuapp.com",
+        }
+      }
+    }
+  }
+};
