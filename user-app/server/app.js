b320722f82de673d187ada25870caa4cbe233a71
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-15 00:55:59 +0530

    Clean server directory and set up for fabric

--- user-app/server/app.js
+++ user-app/server/app.js
@@ -1,41 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var createError = require('http-errors');
+var debug = require('debug')('splidwise:server');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var logger = require('morgan');
+var util = require('util');
 
 var app = express();
+var port = 6401;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(logger('combined'));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// get all the assets in world state
+app.get('/queryAll', async (req, res) => {
+	// invoke fabric to get json with 20 key-val pairs
+	res.send({"hello":"world"});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port);
+debug('Listening on ' + port);
