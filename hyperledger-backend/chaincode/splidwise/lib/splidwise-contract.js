27b5ba7dc6df6833cef47fcd43f32cf5803cadbe
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-16 07:16:54 +0530

    Add all chaincode functions. Descriptive comments to start writing code.

--- hyperledger-backend/chaincode/splidwise/lib/splidwise-contract.js
+++ hyperledger-backend/chaincode/splidwise/lib/splidwise-contract.js
@@ -111,6 +111,62 @@ class SpliDwise extends Contract {
         console.info('============= END : Initialize Ledger ===========');
     }
 
+    // won't need this but this responds with the entire world state: limit to 20?
+    async queryAll(ctx) {
+
+    }
+
+    // get all the assets which are payment links
+    async queryAllLinks(ctx) {
+
+    }
+
+    // get all the assets which are user records
+    async queryAllUsers(ctx) {
+
+    }
+
+    // register a new user
+    // @return: user object
+    async registerUser(ctx) {
+
+    }
+
+    // takes creditor and debtor userids and responds with credit state b/w them
+    // look at creditor's latest txid, get all pmts, do the same for debtor
+    // and continue with calculation
+    async getAmountOwed(ctx) {
+
+    }
+
+    // make payment from creditor to debtor
+    // TODO: check if both are registered (assetExists())
+    // TODO: if payment link exists b/w two then access the latest txid for this debtor
+    // (from creditor user-asset), increment that, and use this new txid for new
+    // payment-link key e.g. (u3,u1,txid+1) where txid was the old value we found in user asset.
+    // TODO: update creditor's latest txid after making pmt. Go to creditor user-asset,
+    // check if debtor is in lent_money_to[], update the txid number;
+    // if not (no link), create a record and append.
+    async makePayment(ctx) {
+
+    }
+
+    // get all payments that creditor made for debtor pending debtor approval
+    // this will be requested by the debtor. assetExists() for both.
+    // if no payment link exists, return empty and explain in message
+    // Notes: we are sending whole payment objects so we can show the details to user (debtor)
+    // before asking for confirmation
+    async getUnapprovedPayments(ctx) {
+
+    }
+
+    // approves an existing payment
+    // inside chaincode, generate the asset key with creditor, debtor, paymentObj.txid
+    // and check if assetExists()
+    async approvePayment(ctx) {
+
+    }
+
 }
 
 module.exports = SpliDwise;
