65efb48084465e381033c31794ddce44256d3665
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-21 06:46:02 +0530

    Setup template for aws deployment - eventually failed since aws only has support for fabric-shim 1.2 and our chaincode is written with fabric-contract-api 1.4 along with fabric-shim 1.4

--- hyperledger-backend/splidwise/startSplidwise.sh
+++ hyperledger-backend/splidwise/startSplidwise.sh
@@ -22,7 +22,14 @@ cd ../basic-network
 # Now launch the CLI container in order to install, instantiate chaincode
 # and submit initLedger txn
 docker-compose -f ./docker-compose.yml up -d cli
-
+### Amazon Managed Blockchain ###
+# docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+# >     -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
+# >     cli peer chaincode install -n splidwise -l node -v 1.0 -p /opt/gopath/src/github.com/splidwise
+# 
+# docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+    # -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
+    # cli peer chaincode instantiate -o $ORDERER -C mychannel -n splidwise -v 1.0 -c '{"Args":[]}' --cafile /opt/home/managedblockchain-tls-chain.pem --tl
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n splidwise -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n splidwise -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
