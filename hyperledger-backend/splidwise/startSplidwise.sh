b345997388e30d7c4381611b0dcea6ca42e79ec6
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-26 00:42:53 +0530

    Add sleep after starting docker

--- hyperledger-backend/splidwise/startSplidwise.sh
+++ hyperledger-backend/splidwise/startSplidwise.sh
@@ -25,15 +25,15 @@ docker-compose -f ./docker-compose.yml up -d cli
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n splidwise -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n splidwise -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
-
-enroll admin and start the server
+sleep 10
+# enroll admin and start the server
 cd ../splidwise
 node services/enrollAdmin.js; sleep 1
 
 echo "Starting splidwise server in a new tmux session [server]..."
 tmux new -d -s server
 tmux send-keys -t server.0 "npm run serve" ENTER
-sleep 5
+sleep 8
 
 echo "Initialising app with users and making dummy payments..."
 for ((c=0; c<=11; c++))
