0f9108f8c53ecccd7afd66e43b2cc0b01fbebd08
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-28 02:04:11 +0530

    Add cors policy and remove wildcard

--- hyperledger-backend/splidwise/app.js
+++ hyperledger-backend/splidwise/app.js
@@ -25,11 +25,26 @@ const rateLimit = rateLimiter({
     headers: true
 });
 
+const allowedOrigins = ['http://localhost:8080',
+                        'http://10.1.22.188:8080'];
+
 app.use(logger('combined'));
 app.use(rateLimit);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // register a new user
 app.post('/registerUser', async (req, res) => {
