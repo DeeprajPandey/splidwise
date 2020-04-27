c79ef6d7357c2b9103374c16ef85289519556d24
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-27 23:53:22 +0530

    Open port on all interfaces so I can access dev site from phone. Also make backend requests to IP addr instead of localhost

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -94,8 +94,9 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
+      host: '0.0.0.0',
       port: 8080,
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // animations: 'all', // --- includes all animations
