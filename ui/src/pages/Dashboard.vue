7026c4e2a1101db0bb44d1705e2b784a0d782e57
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-28 06:02:27 +0530

    Set up frontend for printing data

--- ui/src/pages/Dashboard.vue
+++ ui/src/pages/Dashboard.vue
@@ -1,40 +1,62 @@
 <template>
+  <q-pull-to-refresh @refresh="reload">
   <q-page class="q-pa-lg bg-grey-3 column">
     <div class="q-pa-md" 
     v-if="response.lent_money_to.length > 0">
       <q-list class="rounded-borders bg-white" bordered separator>
         <q-expansion-item expand-separator
         v-for="debtor_arr in response.lent_money_to"
-        :key="debtor_arr[0]">
-        <q-item
-        clickable v-ripple>
+        :key="debtor_arr[0]"
+        @show="debitState(debtor_arr[0])">
+        <template v-slot:header>
+          <q-item-section avatar>
+            <q-avatar icon="perm_identity" color="primary" text-color="white" />
+          </q-item-section>
+
           <q-item-section>
             <q-item-label overline>{{ debtor_arr[1].toUpperCase() }}</q-item-label>
           </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ debtor_arr[0] }}</q-item-label>
-          </q-item-section>
-        </q-item>
+
+          <q-item-section class="desktop-only">{{ debtor_arr[0] }}</q-item-section>
+        </template>
+        <div class="q-pa-md">
+        <div class="row mobile-only" style="margin-top: -2.5vh">
+          <div class="col"></div>
+          <div class="col"><q-item-section style="color: grey" >{{ debtor_arr[0] }}</q-item-section></div>
+        </div>
+        </div>
         </q-expansion-item>
       </q-list>
     </div>
     <div class="q-pa-md" 
     v-if="response.owes_money_to.length > 0">
       <q-list class="rounded-borders bg-white" bordered separator>
-        <q-item
+        <q-expansion-item expand-separator
         v-for="creditor_arr in response.owes_money_to"
         :key="creditor_arr[0]"
-        clickable v-ripple>
+        @show="creditState(creditor_arr[0])">
+        <template v-slot:header>
+          <q-item-section avatar>
+            <q-avatar icon="perm_identity" color="primary" text-color="white" />
+          </q-item-section>
+
           <q-item-section>
             <q-item-label overline>{{ creditor_arr[1].toUpperCase() }}</q-item-label>
           </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ creditor_arr[0] }}</q-item-label>
-          </q-item-section>
-        </q-item>
+
+          <q-item-section class="desktop-only">{{ creditor_arr[0] }}</q-item-section>
+        </template>
+        <div class="q-pa-md">
+        <div class="row mobile-only" style="margin-top: -2.5vh">
+          <div class="col"></div>
+          <div class="col"><q-item-section style="color: grey" >{{ creditor_arr[0] }}</q-item-section></div>
+        </div>
+        </div>
+        </q-expansion-item>
       </q-list>
     </div>
   </q-page>
+  </q-pull-to-refresh>
 </template>
 
 <script>
@@ -42,10 +64,7 @@ import { axiosInstance } from 'boot/axios'
 export default {
   data() {
     return {
-      request_login: {
-        user: "",
-        passw_hash: ""
-      },
+      user: "user1@protonmail.com",
       response: {
         owes_money_to: [],
         lent_money_to: []
@@ -66,7 +85,7 @@ export default {
   },
   methods: {
     loadData() {
-      axiosInstance.post('/user1@protonmail.com/getUser', {
+      axiosInstance.post(`/${this.user}/getUser`, {
         "passw_hash": "hello"
       })
       .then(response => {
@@ -90,14 +109,45 @@ export default {
         });
       })
     },
-    getAmountOwed(user, creditor, debtor) {
-      axiosInstance.post(`/${user}/getAmountOwed`, {
-        "creditor": creditor,
+    debitState(debtor) {
+      axiosInstance.post(`/${this.user}/getAmountOwed`, {
+        "creditor": this.user,
         "debtor": debtor
       })
       .then(response => {
         return response.data
       })
+      .catch(err => {
+        console.log(err.response);
+        this.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: `[${err.response.status}] ${err.response.data.error}`,
+          icon: 'report_problem'
+        });
+      })
+    },
+    creditState(creditor) {
+      axiosInstance.post(`/${this.user}/getAmountOwed`, {
+        "debtor": this.user,
+        "creditor": creditor
+      })
+      .then(response => {
+        return response.data
+      })
+      .catch(err => {
+        console.log(err.response);
+        this.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: `[${err.response.status}] ${err.response.data.error}`,
+          icon: 'report_problem'
+        });
+      })
+    },
+    reload(done) {
+      this.loadData();
+      done();
     }
   }
 }
