b1123ff1bcd2456b3d78faabee0416a9b8cb3bd4
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-28 17:39:20 +0530

    Update sass and state info

---
+++ ui/src/store/splidwise-user/state.js
@@ -0,0 +1,12 @@
+export default function () {
+  return {
+    // user_info: {
+    //   email: 'user1@protonmail.com',
+    //   name: 'Mahavir Jhawar',
+    //   pic_url: ''
+    // }
+    email: 'user1@protonmail.com',
+    name: 'Mahavir Jhawar',
+    pic_url: ''
+  }
+}
