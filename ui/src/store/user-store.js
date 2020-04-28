b2becc5f7b7cf30e12588da0c95e4f0a69c9572d
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-29 01:02:39 +0530

    Add getters, actions & mutations for updating and getting all info in store

--- ui/src/store/user-store.js
+++ ui/src/store/user-store.js
@@ -1,28 +1,36 @@
 const state = {
   username: 'user1@protonmail.com',
   name: 'Mahavir Jhawar',
-  owes_money_to: ['user3@ashoka.edu'],
   lent_money_to: [],
+  owes_money_to: ['user3@ashoka.edu']
   pic_url: ''
 }
 
 const mutations = {
-  updateLent(state, payload_arr) {
+  setInfo(state, payload_obj) {
+    state.username = payload_obj.username;
+    state.name = payload_obj.name;
+    state.lent_money_to = payload_obj.lent_money_to;
+    state.owes_money_to = payload_obj.owes_money_to;
+    state.pic_url = payload_obj.pic_url;
+  },
+  setLent(state, payload_arr) {
     state.lent_money_to = payload_arr;
   },
-  updateOwes(state, payload_arr) {
+  setOwes(state, payload_arr) {
     state.owes_money_to = payload_arr;
   }
 }
 
 const actions = {
+  updateUserData({ commit }, payload) {
+    commit('setInfo', payload);
+  },
   updateLentArr({ commit }, payload) {
-    commit('updateLent', payload)
-    return;
+    commit('setLent', payload);
   },
   updateOwesArr({ commit }, payload) {
-    commit('updateOwes', payload)
-    return;
+    commit('setOwes', payload);
   }
 }
 
@@ -30,11 +38,17 @@ const getters = {
   uname: (state) => {
     return state.username;
   },
+  name: (state) => {
+    return state.name;
+  },
   lent_money_to: (state) => {
     return state.lent_money_to;
   },
   owes_money_to: (state) => {
     return state.owes_money_to;
+  },
+  pic_url: (state) => {
+    return state.pic_url;
   }
 }
 
