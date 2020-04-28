bb8514dbb2ef18ad22648882a1d35f3f6182d923
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-28 07:54:52 +0530

    Add icon component

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -52,7 +52,10 @@ module.exports = function (/* ctx */) {
       //            (not treeshaking Quasar; biggest bundle size; convenient)
       all: 'auto',
 
-      components: ['QBtn'],
+      components: [
+        'QBtn',
+        'QIcon'
+      ],
       directives: [],
       
       // config: {
