1016b63cb0d6251244ec53884010f191c3eb0c8f
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-30 18:56:54 +0530

    Clear user data and call logout endpoint on logout

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           flat
           icon='exit_to_app'
           label='Logout'
-          to = '/'
+          @click="logout"
         />
         <!-- <q-toolbar-title>
           SPLIDWISE
@@ -120,18 +120,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { date } from 'quasar'
-import EssentialLink from 'components/EssentialLink'
 
 export default {
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
-
   computed: {
+    ...mapActions('user_info', [
+      'clearUserData'
+    ]),
     ...mapGetters('user_info', [
       'name', 'pic_url'
     ]),
@@ -143,51 +141,15 @@ export default {
 
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
-      ]
+      leftDrawerOpen: false
+    }
+  },
+
+  methods: {
+    logout() {
+      this.clearUserData;
+      // change this to logout endpoint on server
+      this.$router.push('/');
     }
   }
 }
